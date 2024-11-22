3f5e0128aaa3576a91f86c6eb1c9d43f67fd31f3
Alexandre Alves <alexandre.alves@suse.com>
2024-11-22 14:47:13 +0000

    add new dialog so that users dont forget final voting

---
+++ src/components/NeedFinalVoteDialog.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  name: "ResetIssueDialog",
+  props: {
+    showNeedFinalVoteDialog: {
+      type: Boolean,
+      default: () => false,
+    },
+    issueId: {
+      type: [Number, String],
+      default: () => "",
+    },
+  },
+  computed: {
+    dialogVisibility: {
+      // getter
+      get: function () {
+        return this.showNeedFinalVoteDialog;
+      },
+      // setter
+      set: function () {},
+    },
+  },
+  methods: {
+    ok() {
+      this.$emit("updateShowNeedFinalVoteDialog", false);
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <v-dialog v-model="dialogVisibility" width="500" persistent>
+      <div class="dialog-content">
+        <h3>Final vote needed!</h3>
+        <p class="dialog-text">
+          <span class="dialog-text__line">
+            You need to cast the final vote before closing the estimation
+          </span>
+        </p>
+        <div class="dialog-controls">
+          <v-btn outlined class="btn-secondary" @click="ok">ok</v-btn>
+        </div>
+      </div>
+    </v-dialog>
+  </div>
+</template>
